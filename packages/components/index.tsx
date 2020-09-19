06473a726689f665de0e06a4021428cda9171e39
Fernando Rojo <frojo@sas.upenn.edu>
2020-09-19 19:51:26 -0400

    clear out

--- packages/components/index.tsx
+++ packages/components/index.tsx
@@ -6,5 +6,5 @@ type Props = {};
 export default function BaseComponent(props: Props) {
   const {} = props;
 
-  return <View style={{ backgroundColor: "blue", height: 500, width: 500 }} />;
+  return <View style={{ backgroundColor: "red", height: 500, width: 500 }} />;
 }
